eab4b561b67d673701322cd6ce329c80d7f88aca
Shiounsama <tomio.ciesielski@gmail.com>
2024-10-04 14:06:29 +0200

    CAMERA RENDUUU OUUU ??

--- Assets/Script/TestCam2.cs
+++ Assets/Script/TestCam2.cs
@@ -4,12 +4,24 @@ using UnityEngine;
 
 public class TestCam2 : MonoBehaviour
 {
-    public float moveSpeed = 0.1f;
-    public float zoomSpeed = 5f; 
-    public float minZoom = 5f;    
-    public float maxZoom = 15f;
+    public float moveSpeed = 0.1f;    
+    public float smoothSpeed = 5f;    
+    public float zoomSpeed = 5f;      
+    public float minZoom = 5f;        
+    public float maxZoom = 15f;       
 
     private Vector3 dragOrigin;
+    private Vector3 targetPosition;
+
+    public GameObject LEMONDE;
+
+    public Camera camIso;
+
+    void Start()
+    {
+        camIso = GetComponent<Camera>();
+        targetPosition = LEMONDE.transform.position; 
+    }
 
     void Update()
     {
@@ -19,23 +31,65 @@ public class TestCam2 : MonoBehaviour
             dragOrigin = Input.mousePosition; 
         }
 
-        if (Input.GetMouseButton(0)) 
+        if (Input.GetMouseButton(0))
         {
             Vector3 difference = dragOrigin - Input.mousePosition; 
             dragOrigin = Input.mousePosition;
 
-            Vector3 move = new Vector3(difference.x * moveSpeed, 0, difference.y * moveSpeed);
-
+           
+            Vector3 move = new Vector3(-difference.x * moveSpeed, 0, -difference.y * moveSpeed);
             Vector3 moveDirection = Quaternion.Euler(0, transform.eulerAngles.y, 0) * move;
 
-            transform.Translate(moveDirection, Space.World);
+            
+            targetPosition += moveDirection;
         }
 
+        
+        LEMONDE.transform.position = Vector3.Lerp(LEMONDE.transform.position, targetPosition, Time.deltaTime * smoothSpeed);
+
+        if (camIso.orthographic)
+        {
+            camIso.orthographicSize = Mathf.Clamp(camIso.orthographicSize - Input.GetAxis("Mouse ScrollWheel") * zoomSpeed, minZoom, maxZoom);
+        }
+        else
+        {
+            camIso.fieldOfView = Mathf.Clamp(camIso.orthographicSize - Input.GetAxis("Mouse ScrollWheel") * zoomSpeed, minZoom, maxZoom);
+        }
+
+        if (camIso.orthographicSize > 7)
+        {
+            moveSpeed = 0.1f;
+        }
+        else
+        {
+            moveSpeed= 0.05f;
+        }
+
+        ZoomAtMousePosition();
+    }
+
+    void ZoomAtMousePosition()
+    {
         float scroll = Input.GetAxis("Mouse ScrollWheel");
         if (scroll != 0)
         {
-            Camera.main.orthographicSize -= scroll * zoomSpeed;
-            Camera.main.orthographicSize = Mathf.Clamp(Camera.main.orthographicSize, minZoom, maxZoom);
+            Vector3 mousePos = Input.mousePosition;
+            Vector3 worldPointBeforeZoom = camIso.ScreenToWorldPoint(new Vector3(mousePos.x, mousePos.y, camIso.nearClipPlane));
+
+            if (camIso.orthographic)
+            {
+                camIso.orthographicSize = Mathf.Clamp(camIso.orthographicSize - scroll * zoomSpeed, minZoom, maxZoom);
+            }
+            else
+            {
+                camIso.fieldOfView = Mathf.Clamp(camIso.fieldOfView - scroll * zoomSpeed, minZoom, maxZoom);
+            }
+
+            Vector3 worldPointAfterZoom = camIso.ScreenToWorldPoint(new Vector3(mousePos.x, mousePos.y, camIso.nearClipPlane));
+
+            Vector3 difference = worldPointBeforeZoom - worldPointAfterZoom;
+
+            camIso.transform.position += difference;
         }
     }
 }
