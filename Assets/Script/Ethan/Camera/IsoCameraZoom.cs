9e9c50cd2464aae18fd267b389eee6438e3853c4
XProKio <roger.paldi@yahoo.com>
2024-10-24 23:07:05 +0200

    Rework Camera Behaviour

--- Assets/Script/Ethan/Camera/IsoCameraZoom.cs
+++ Assets/Script/Ethan/Camera/IsoCameraZoom.cs
@@ -1,8 +1,39 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class IsoCameraZoom : MonoBehaviour
 {
+    public Transform objectToMove;
+
+    private Vector3 directionToMove;
+    private Vector3 zoomTargetPosition;
+
+    public float zoomSpeed;
+    public float zoomSensitivity;
+
+    public float zoomCooldown;
+    private float zoomCooldownValue;
+
+    private void Update()
+    {
+        if (zoomCooldownValue >= 0)
+        {
+            zoomCooldownValue -= Time.deltaTime;
+            objectToMove.position = Vector3.Lerp(objectToMove.position, zoomTargetPosition, zoomSpeed * Time.deltaTime);
+        }
+    }
+
     // == Public Methodes == \\
+
+    public void OnZoom(InputAction.CallbackContext context)
+    {
+        if (context.performed && zoomCooldownValue <= 0)
+        {
+            zoomCooldownValue = zoomCooldown;
+            zoomTargetPosition = objectToMove.position + transform.forward * context.ReadValue<float>() / zoomSensitivity;
+        }
+    }
+
     // == Private Methodes == \\
     // == Shortcut Methodes == \\
 }
