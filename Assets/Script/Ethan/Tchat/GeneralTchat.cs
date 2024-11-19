7b5005c5c449eafcc5e1b46e0b355509fc896b03
D'alessandro Ethan <roger.paldi@yahoo.com>
2024-11-19 11:25:54 +0100

    YIAAAAAAAAA ça fonctionne

--- Assets/Script/Ethan/Tchat/GeneralTchat.cs
+++ Assets/Script/Ethan/Tchat/GeneralTchat.cs
@@ -1,7 +1,5 @@
-using UnityEngine;
-using NaughtyAttributes;
-using UnityEngine.UI;
 using TMPro;
+using UnityEngine;
 
 public class GeneralTchat : MonoBehaviour
 {
@@ -11,7 +9,7 @@ public class GeneralTchat : MonoBehaviour
     public GameObject newMessagePrefab;
 
     public Transform canvasTransform;
-    
+
     public void Start()
     {
         //Je Clear le tchat pour être sur de ne pas avoir déjà des residus de messages lors du début du jeu
@@ -25,12 +23,15 @@ public class GeneralTchat : MonoBehaviour
 
     private void createMessage()
     {
-        //J'instancie un actualMessage dans un endroit précis
-        GameObject actualMessage = Instantiate(newMessagePrefab, canvasTransform.position, Quaternion.identity, canvasTransform);
-        //Je modifie le contenue du text de mon prefab à celui qui correspond
-        actualMessage.GetComponent<TextMeshProUGUI>().text = generalTchat.listOfMessage[messageCount];
-        //J'incremente MessageCount qui va servir d'index pour se balader dans la liste des message de GeneralTchat
-        messageCount++;
+        if (messageCount < generalTchat.listOfMessage.Count)
+        {
+            //J'instancie un actualMessage dans un endroit précis
+            GameObject actualMessage = Instantiate(newMessagePrefab, canvasTransform.position, Quaternion.identity, canvasTransform);
+            //Je modifie le contenue du text de mon prefab à celui qui correspond
+            actualMessage.GetComponent<TextMeshProUGUI>().text = generalTchat.listOfMessage[messageCount];
+            //J'incremente MessageCount qui va servir d'index pour se balader dans la liste des message de GeneralTchat
+            messageCount++;
+        }
     }
 
     private void ClearTchat()
