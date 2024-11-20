530d13d055452cfc80b538869d738cc491a4b233
D'alessandro Ethan <roger.paldi@yahoo.com>
2024-11-20 11:38:28 +0100

    Starting Emoji

---
+++ Assets/Script/Ethan/ScriptableObjects/Tchat/GeneralEmoji.cs
@@ -0,0 +1,8 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "My Asset/GeneralEmoji")]
+public class GeneralEmoji : ScriptableObject
+{
+    public List<EmojiID> listOfEmoji;
+}
