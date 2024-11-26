12521bf43a0558394b80e1d847ff9d3e3f762e40
Sybirk <115795460+Sybirk@users.noreply.github.com>
2024-11-26 11:05:18 +0100

    InGameSeeker UXUI

---
+++ Assets/Script/Oscar/ConfirmGuess.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ConfirmGuess : MonoBehaviour
+{
+    public void Confirm()
+    {
+        Debug.Log("Guess");
+    }
+}
