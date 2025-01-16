fbe6691daae3c3c1a11d00227d625213baee9773
D'alessandro Ethan <roger.paldi@yahoo.com>
2025-01-16 09:51:38 +0100

    fix freeze camera

--- Assets/Script/Camera/FreezeDragMovement.cs
+++ Assets/Script/Camera/FreezeDragMovement.cs
@@ -1,39 +1,52 @@
 using Mirror;
+using System;
 using UnityEngine;
 using UnityEngine.EventSystems;
 
 public class FreezeDragMovement : NetworkBehaviour, IEndDragHandler, IBeginDragHandler
 {
     public Transform cameraToFreeze;
+    private bool isIso;
 
     public void OnBeginDrag(PointerEventData eventData)
     {
         cameraToFreeze = Camera.main.transform;
 
-        if(cameraToFreeze != null )
+        if (cameraToFreeze != null)
         {
-            if (cameraToFreeze.GetComponent<Camera360>() != null)
+            if (CheckComponent(typeof(Camera360)) && cameraToFreeze.GetComponent<Camera360>().enabled == true)
             {
+                isIso = false;
                 cameraToFreeze.GetComponent<Camera360>().enabled = false;
             }
-            if (cameraToFreeze.GetComponent<IsoCameraDrag>() != null)
+            if (CheckComponent(typeof(IsoCameraDrag)) && cameraToFreeze.GetComponent<IsoCameraDrag>().enabled == true)
             {
+                isIso = true;
                 cameraToFreeze.GetComponent<IsoCameraDrag>().enabled = false;
             }
         }
     }
     public void OnEndDrag(PointerEventData eventData)
     {
-        if(cameraToFreeze != null)
+        if (cameraToFreeze != null)
         {
-            if (cameraToFreeze.GetComponent<Camera360>() != null)
+            if (CheckComponent(typeof(Camera360)) && !isIso)
             {
                 cameraToFreeze.GetComponent<Camera360>().enabled = true;
             }
-            if (cameraToFreeze.GetComponent<IsoCameraDrag>() != null)
+            if (CheckComponent(typeof(IsoCameraDrag)) && isIso)
             {
                 cameraToFreeze.GetComponent<IsoCameraDrag>().enabled = true;
             }
         }
+    }
+
+    public bool CheckComponent(Type behaviourType)
+    {
+        if (cameraToFreeze.GetComponent(behaviourType) != null)
+        {
+            return true;
+        }
+        return false;
     }
 }
