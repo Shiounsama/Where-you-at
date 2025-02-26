8e7c798e1679e4f639ab2f7e3d5ba58ff9fa5a9b
XProKio <roger.paldi@yahoo.com>
2025-02-26 18:40:54 +0100

    X-Ray du bled parce que faut pas toucher au URP

---
+++ Assets/Script/Camera/IsoCameraXRay.cs
@@ -0,0 +1,41 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IsoCameraXRay : MonoBehaviour
+{
+    private List<GameObject> allGameObjectInVision = new List<GameObject>();
+
+    public LayerMask layerToCheck;
+
+    public Vector3 boxSize;
+
+    public float minSizeBeforeXRay;
+
+    public void GetAllGameObjectInVision()
+    {
+        if (allGameObjectInVision.Count > 0)
+        {
+            SetVisibilityOfObjects(true);
+            allGameObjectInVision.Clear();
+        }
+        if (Camera.main.orthographicSize < minSizeBeforeXRay)
+        {
+            foreach (RaycastHit col in Physics.BoxCastAll(transform.position, boxSize, transform.forward, Quaternion.identity, 1000f, layerToCheck))
+            {
+                if (!allGameObjectInVision.Contains(col.collider.transform.gameObject))
+                {
+                    allGameObjectInVision.Add(col.collider.transform.gameObject);
+                }
+            }
+            SetVisibilityOfObjects(false);
+        }
+    }
+
+    public void SetVisibilityOfObjects(bool isVisible)
+    {
+        foreach (GameObject go in allGameObjectInVision)
+        {
+            go.SetActive(isVisible);
+        }
+    }
+}
