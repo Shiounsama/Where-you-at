70366fae1196ec667745811185e3e89ee463dc58
Shiounsama <tomio.ciesielski@gmail.com>
2025-03-06 10:52:09 +0100

    Essaie pour le iTween

--- Assets/Script/CarMove.cs
+++ Assets/Script/CarMove.cs
@@ -5,15 +5,43 @@ using DG.Tweening;
 
 public class CarMove : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public List<Transform> pointsDirection;
+    public float temps = 5f; 
+    public bool loop = true; 
+
+    private void Start()
     {
-        
+        MoveAlongPath();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void MoveAlongPath()
     {
-        
+        Vector3[] pathPositions = new Vector3[pointsDirection.Count];
+        for (int i = 0; i < pointsDirection.Count; i++)
+        {
+            pathPositions[i] = pointsDirection[i].position;
+        }
+
+        iTween.MoveTo(gameObject, iTween.Hash(
+            "path", pathPositions,
+            "time", temps,
+            "orienttopath", true,
+            "easetype", iTween.EaseType.linear,
+            "looptype", loop 
+        ));
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.green;
+        for (int i = 0; i < pointsDirection.Count - 1; i++)
+        {
+            Gizmos.DrawLine(pointsDirection[i].position, pointsDirection[i + 1].position);
+        }
+
+        if (loop && pointsDirection.Count > 1)
+        {
+            Gizmos.DrawLine(pointsDirection[pointsDirection.Count - 1].position, pointsDirection[0].position);
+        }
     }
 }
