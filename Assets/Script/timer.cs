488486dfc5bce36384bdb2bba7c105040d8345dc
Shiounsama <tomio.ciesielski@gmail.com>
2025-04-01 21:56:16 +0200

    Update timer

--- Assets/Script/timer.cs
+++ Assets/Script/timer.cs
@@ -11,7 +11,10 @@ public class timer : NetworkBehaviour
 {
     public timer tempsjoueur;
     public int time = 0;
+    public int timeLost = 0;
+
     public int timeStart = 180;
+    public int timeStartLost = 150;
 
     public IEnumerator Timer()
     {
@@ -19,10 +22,12 @@ public class timer : NetworkBehaviour
         TMP_Text texteTimer = GetComponentInChildren<TMP_Text>();
 
         time = timeStart;
+        timeLost = timeStartLost;
         texteTimer.enabled = true;
         while (tempsjoueur.time > 0)
         {
             tempsjoueur.time--;
+            tempsjoueur.timeLost--;
             yield return new WaitForSeconds(1f);
             texteTimer.text = string.Format("{0:0}:{1:00}", Mathf.Floor(tempsjoueur.time / 60), tempsjoueur.time % 60);
         }
