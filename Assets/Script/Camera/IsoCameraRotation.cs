2b5f93eaa442bc0577b0c76873ec979849a3c374
D'alessandro Ethan <roger.paldi@yahoo.com>
2025-04-02 17:26:57 +0200

    Fix Camera Drag and rotate

--- Assets/Script/Camera/IsoCameraRotation.cs
+++ Assets/Script/Camera/IsoCameraRotation.cs
@@ -78,6 +78,7 @@ public class IsoCameraRotation : MonoBehaviour
         GetComponent<IsoCameraDrag>().enabled = false;
         GetComponent<IsoCameraDrag>().isDragging = false;
 
+
         Vector3 startPosition = cam.transform.position;
 
         float elapsedTime = 0f;
@@ -98,6 +99,9 @@ public class IsoCameraRotation : MonoBehaviour
 
 
         isTransitioning = false;
+        CamDrag.startDraggingMousePos = CamDrag.GetMouseWorldPosition();
+        CamDrag.cameraPosOrigin = cam.transform.position;
+        CamDrag.objectOriginPos = CamDrag.objectToMove.position;
         GetComponent<IsoCameraDrag>().enabled = true;
 
     }
