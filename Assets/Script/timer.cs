00ef5f9c51b90d152a3423124ea82dd669aa166b
Shiounsama <tomio.ciesielski@gmail.com>
2025-04-02 16:34:40 +0200

    Fete foraine dans la vlle

--- Assets/Script/timer.cs
+++ Assets/Script/timer.cs
@@ -11,10 +11,8 @@ public class timer : NetworkBehaviour
 {
     public timer tempsjoueur;
     public int time = 0;
-    public int timeLost = 0;
 
     public int timeStart = 180;
-    public int timeStartLost = 150;
 
     public IEnumerator Timer()
     {
@@ -22,14 +20,10 @@ public class timer : NetworkBehaviour
         TMP_Text texteTimer = GetComponentInChildren<TMP_Text>();
 
         time = timeStart;
-        timeLost = timeStartLost;
         texteTimer.enabled = true;
         while (tempsjoueur.time > 0)
         {
             tempsjoueur.time--;
-            if(tempsjoueur.timeLost > 0)
-                tempsjoueur.timeLost--;
-
             yield return new WaitForSeconds(1f);
             texteTimer.text = string.Format("{0:0}:{1:00}", Mathf.Floor(tempsjoueur.time / 60), tempsjoueur.time % 60);
         }
