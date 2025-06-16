4da989b401ae3f4c67c95628ac4678a3258099b3
Vallérian Dicque <83017594+cidevalgames@users.noreply.github.com>
2025-06-16 05:43:37 +0200

    Ajout des SFX sur les boutons dans le jeu

--- Assets/Script/Sound Design/Buttons/SendMessageButtonAudio.cs
+++ Assets/Script/Sound Design/Buttons/SendMessageButtonAudio.cs
@@ -2,17 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SendMessageButtonAudio : MonoBehaviour
+public class SendMessageButtonAudio : ButtonAudio
 {
-    // Start is called before the first frame update
-    void Start()
+    public override void Start()
     {
-        
-    }
+        base.Start();
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        buttonClickClip = SoundBank.buttonClick_SendMessage_Clip;
     }
 }
